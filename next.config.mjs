3adb8ca0d88eacd753fc856fa5189fd089f0dd95
ArtemYerokhin-dev <artem.yerokhin1412@gmail.com>
2026-04-30 20:16:54 +0200

    configure github pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/quickcart',
-
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'raw.githubusercontent.com',
-                pathname: '**',
-            },
-        ],
-    },
+  output: 'export',
+  turbopack: {
+    root: __dirname,  // ← добавь это
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
